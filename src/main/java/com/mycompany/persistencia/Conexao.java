9f9f204e1d38733fb034719e6b4021f79862adff
Dev <Dev@192.168.0.38>
2019-04-27 10:53:51 -0300

    Classe FilmeDAO feita - Falta testar

---
+++ src/main/java/com/mycompany/persistencia/Conexao.java
@@ -0,0 +1,47 @@
+package com.mycompany.persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    
+    private static final String USUARIO = "hzuzmblqwbnnlt";
+    private static final String SENHA = "bdb8cca23afc5e7dd0f7d734a58653d1179d8b466ea73be0236227c38d1ba05b";
+    private static final String CAMINHO = "jdbc:postgresql://ec2-54-235-114-242.compute-1.amazonaws.com:5432/d31o9kgkqrorvi";
+    private static final String DRIVER = "org.postgresql.Driver";
+    
+    //jdbc:postgresql://SERVIDOR:PORTA/BANCO?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory
+
+    private static Connection connection;
+    
+    public static void dbConnection() {
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException ex) {
+            //Logger.getLogger(PVendedor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            connection = DriverManager.getConnection(CAMINHO, USUARIO, SENHA);
+            System.out.println("Connected");
+        } catch (SQLException ex) {
+           //Logger.getLogger(PVendedor.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Not connected");
+        }
+
+    }
+    
+    public static void closeConnection(){
+        try {
+            connection.close();
+        } catch (SQLException ex) {
+            //Logger.getLogger(PConexao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+  
+    public static Connection getConexao(){
+        return connection;
+    }
+}
+
+
---
+++ src/main/java/com/mycompany/persistencia/FilmeDAO.java
@@ -0,0 +1,77 @@
+
+package com.mycompany.persistencia;
+
+import com.mycompany.modelo.Filme;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author Dev
+ */
+public class FilmeDAO {
+    
+    private final String INSERTFILME = "INSERT INTO FILMES (TITULO, DATA_LANCAMENTO, NOTA, DESCRICAO, QUANTIDADE) VALUES (?,?,?,?,?)";
+    private final String DELETEFILME = "DELETE FROM FILMES WHERE ID_FILME = ?";
+    private final String LISTAFILME = "SELECT * FROM FILMES WHERE ORDER BY";
+    
+    public boolean inserirFilme(Filme filme){
+        
+        Conexao.dbConnection();
+        
+        try {
+            
+            PreparedStatement preparaInstrucao;
+            preparaInstrucao = Conexao.getConexao().prepareStatement(INSERTFILME);
+            
+            preparaInstrucao.setString(1, filme.getTitulo());
+            preparaInstrucao.setDate(2, (Date) filme.getDatalancamento());
+            preparaInstrucao.setInt(3, filme.getNota());
+            preparaInstrucao.setString(4, filme.getDescricao());
+            preparaInstrucao.setInt(5, filme.getQuantidade());
+            
+            Conexao.closeConnection();
+            return true;
+        } catch (SQLException ex) {
+            System.err.println(ex);
+            ex.printStackTrace();
+            return false;
+        }
+        
+    }
+    
+    /**
+     *
+     * @return
+     */
+    public static ArrayList<Filme> list(){
+        
+        ArrayList<Filme> lista = new ArrayList<Filme>();
+        Conexao.dbConnection();
+        String query = "SELECT * FROM FILMES WHERE ORDER BY";
+        
+        try {
+            PreparedStatement pst = Conexao.getConexao().prepareStatement(query);
+            ResultSet rs = pst.executeQuery();
+            
+            while(rs.next()){
+                Filme f = new Filme(
+                    rs.getString("TITULO"),
+                    rs.getDate("DATA_ENTRADA"), 
+                    rs.getInt("NOTA"),
+                    rs.getString("DESCRICAO"),
+                    rs.getInt("QUANTIDADE"));  
+                lista.add(f);
+            }  
+            Conexao.closeConnection();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        return lista;
+    }
+    
+}
