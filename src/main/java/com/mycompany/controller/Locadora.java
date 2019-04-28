33c574b302bf41e8a6de0eae0b61cd42c3817697
Dev <Dev@192.168.0.38>
2019-04-28 19:52:39 -0300

    Atualizações que não mudaram muita coisa mas vdc.

--- src/main/java/com/mycompany/controller/Locadora.java
+++ src/main/java/com/mycompany/controller/Locadora.java
@@ -7,7 +7,11 @@ package com.mycompany.controller;
 
 import com.mycompany.modelo.Filme;
 import com.mycompany.persistencia.FilmeDAO;
+import static com.sun.corba.se.spi.presentation.rmi.StubAdapter.request;
 import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -22,14 +26,20 @@ public class Locadora {
    
     private ArrayList<Filme> lista = null;
     private Filme filme;
-    private  FilmeDAO filmes = new FilmeDAO();
+    private FilmeDAO filmes = new FilmeDAO();
     
+    private String titulo;
+    private Date data;
+    private Integer nota;
+    private String descricao;
+    private Integer quantidade;
     
-    public Locadora() {
+
+    public Locadora() throws ParseException {
         
-        Date data = Date.valueOf("2018-10-19");
-        filme = new Filme("Mogli",data,3,"O menino Lobo", 20);
-        
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+java.   sql.Date datas = new java.sql.Date(df.parse("000-00-00").getTime());
+        filme = new Filme("Titulo do Filme",datas,0,"Descricao",0);
     }
     
     public ArrayList<Filme> getLista() {
@@ -53,24 +63,69 @@ public class Locadora {
         return lista;
     } 
     
-    public void adicionarFilme(String titulo, Date data_lancamento, int nota, String descricao,int quantidade){
-        
-        filme.setTitulo(titulo);
-        filme.setData_lancamento(data_lancamento);
-        filme.setNota(nota);
-        filme.setDescricao(descricao);
-        filme.setQuantidade(quantidade);
-        
-        filmes.inserirFilme(filme.getTitulo(), (Date) filme.getData_lancamento(), filme.getNota(), filme.getDescricao(), filme.getQuantidade());
-        
-    }
-
     public Filme getFilme() {
         return filme;
     }
 
     public void setFilme(Filme filme) {
         this.filme = filme;
+    }
+
+    public void setFilmes(FilmeDAO filmes) {
+        this.filmes = filmes;
+    }
+    
+    public void adicionarFilme(String titulo, Date data_lancamento, int nota, String descricao,int quantidade){
+
+        filme.setTitulo(titulo);
+        filme.setData_lancamento(data_lancamento);
+        filme.setNota(nota);
+        filme.setDescricao(descricao);
+        filme.setQuantidade(quantidade);
+        
+        System.out.println(filme.getDescricao() + filme.getData_lancamento() + filme.getNota() + filme.getDescricao() + filme.getQuantidade());
+        filmes.inserirFilme(filme.getTitulo(), filme.getData_lancamento(), filme.getNota(), filme.getDescricao(), filme.getQuantidade());
+    
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Integer getNota() {
+        return nota;
+    }
+
+    public void setNota(Integer nota) {
+        this.nota = nota;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
     }
     
     
--- src/main/java/com/mycompany/modelo/Filme.java
+++ src/main/java/com/mycompany/modelo/Filme.java
@@ -5,7 +5,7 @@
  */
 package com.mycompany.modelo;
 
-import java.util.Date;
+import java.sql.Date;
 
 
 /**
